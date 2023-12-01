8f7e942fe620b419c0357585db4d8e8f95cc0e21
Becky@EDITH <becky.shaw2@sainsburys.co.uk>
2023-12-01 09:15:02 +0000

    Chapter3 Updates

---
+++ Codedex/Chapter3/grades.py
@@ -0,0 +1,6 @@
+grade = int(input('What is the grade received? '))
+if grade >= 75:
+    print('Pass mark achieved')
+else:
+    print('Passing mark not achieved')
+    print("current grade: ", (grade) )
---
+++ Codedex/Chapter3/ph_levels.py
@@ -0,0 +1,8 @@
+pH = int(input('What is the pH level? '))
+
+if pH > 7:
+    print('pH level is basic')
+elif pH < 7:
+    print('pH is acidic')
+else:
+    print('pH is neutral')
