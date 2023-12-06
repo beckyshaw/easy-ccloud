62679563fc30ef180a492afe392e287b0fa38981
becky.shaw2 <becky.shaw2@sainsburys.co.uk>
2023-12-06 15:15:54 +0000

    updated for ccloud and general updated o7

---
+++ ccloud/prod/tasks/api-csv.sh
@@ -0,0 +1,45 @@
+# #!/bin/bash
+# Variable file
+source vars/stg-env.sh
+
+# Current-context verification
+if [[ $CURR_CTXT =~ $ENV ]]; then
+echo "Environment is $ENV"
+echo " Current context is $CURR_CTXT"
+echo -------------------------------
+echo "Date: $datestring"
+echo "Environment and context match"
+echo -------------------------------
+
+else
+    echo ---------------------------------------------------------------
+    echo "Context/Env mismatch! Context: $CURR_CTXT, ENV: $ENV"
+    echo ---------------------------------------------------------------
+    echo " Select kube context : "
+    PS3="Select context: "
+    select NEW_CTXT in core-dev confluent-nonprod confluent-prod
+    do
+        echo "Selected context: $CONTEXT"   
+        sleep 3
+        echo "$(kubectl config use-context $NEW_CTXT)" 
+        break;
+    done
+echo -------------------------------
+echo " New context is: $NEW_CTXT"   
+echo -------------------------------
+fi
+
+read -p " Enter client name for API retrieval :  " clientName
+echo " You have chosen $clientName " 
+read -p " Continue? (y/n) " CONT
+if [ "$CONT" = "yes" ] || [ "$CONT" = "y" ] || [ "CONT" = "Yes" ]; then
+    echo -------------------------------------------------------------------
+    echo "exporting api keys for $clientName "
+        eval " confluent api-key list | grep $clientName > nonprod/key-export/$clientName.csv "
+        echo " Complete. File location nonprod/key-export/$clientName.csv "
+        eval "cat nonprod/key-export/$clientName.csv"
+    echo -------------------------------------------------------------------
+        echo "Complete..."
+else
+    echo "Exiting....."
+fi
---
+++ ccloud/prod/tasks/delete-secrets.sh
@@ -0,0 +1,60 @@
+# #!/bin/bash
+# Variable file
+source vars/prod-env.sh
+
+# Current-context verification
+if [[ $CURR_CTXT =~ $ENV ]]; then
+echo "Environment is $ENV"
+echo " Current context is $CURR_CTXT"
+echo -------------------------------
+echo "Date: $datestring"
+echo "Environment and context match"
+echo -------------------------------
+
+else
+    echo ---------------------------------------------------------------
+    echo "Context/Env mismatch! Context: $CURR_CTXT, ENV: $ENV"
+    echo ---------------------------------------------------------------
+    echo " Select kube context : "
+    PS3="Select context: "
+    select NEW_CTXT in core-dev confluent-nonprod confluent-prod
+    do
+        echo "Selected context: $CONTEXT"   
+        sleep 3
+        echo "$(kubectl config use-context $NEW_CTXT)" 
+        break;
+    done
+echo -------------------------------
+echo "New context is: $NEW_CTXT"   
+echo -------------------------------
+fi
+
+echo ---------------------------------------------------------------------------
+echo " Datetime: $datestring "
+# echo " Logging into Confluent Cloud"
+# eval "confluent login"
+#     sleep 5
+
+echo "API-Key name translates to file name containing api-keys"
+read -p " Please enter the api key name... " CLIENT_FILE
+
+        KEY_DEL="confluent api-key delete"
+
+echo "Selected file is $CLIENT_FILE.."
+echo ---------------------------------------------------------------------------
+echo "You have selected the following api-keys to delete: "
+eval " cat prod/env/$CLIENT_FILE "
+echo ---------------------------------------------------------------------------
+read -p  " Is this correct ? (yes/no) " CONT
+    if [ "$CONT" = "yes" ] || [ "$CONT" = "y" ] || [ "CONT" = "Yes" ]; then
+        echo ---------------------------------------------------------------
+        echo "Deleting keys .... "
+            while read -r line;
+            do  
+                [[ -n "$line" ]] && yes | eval "confluent api-key delete $line";
+            done < "prod/env/$CLIENT_FILE"
+        echo ---------------------------------------------------------------
+    
+    else
+        echo " Exiting.....";
+    fi
